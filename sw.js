4f0da458346c6356b30a64416f0464fb01ae486c
SHOUVIK BAIDYA <162677655+Baidya1483@users.noreply.github.com>
2026-09-03 17:48:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'huemore-v2';
+const CACHE = 'huemore-v5';
 const CORE = ['/', '/index.html', '/manifest.webmanifest', '/favicon.png', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
